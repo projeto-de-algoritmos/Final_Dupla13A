f764d4a495a06b35367cf18d80b7d51fd7196840
guilherme-mendes <guimendesp12@gmail.com>
2020-12-07 11:32:48 -0300

    Add itens images

---
+++ src/auxiliary.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+BRONZE = pygame.transform.scale(pygame.image.load('../images/itens/bronze.png'), (22, 22))
+DIAMOND = pygame.transform.scale(pygame.image.load('../images/itens/diamond.png'), (22, 22))
+GOLD = pygame.transform.scale(pygame.image.load('../images/itens/gold.png'), (22, 22))
+IRON = pygame.transform.scale(pygame.image.load('../images/itens/iron.png'), (22, 22))
+RUBY = pygame.transform.scale(pygame.image.load('../images/itens/ruby.png'), (22, 22))
+SILVER = pygame.transform.scale(pygame.image.load('../images/itens/silver.png'), (22, 22))
+WOOD = pygame.transform.scale(pygame.image.load('../images/itens/wood.png'), (22, 22))
